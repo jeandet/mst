a5b77c949c9e5872911ca0839fb1b9e4344ce412
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-11-26 15:39:39 +0100

    Devices tag cleanup
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- ucpp/peripherals_tags.hpp
+++ ucpp/peripherals_tags.hpp
@@ -5,9 +5,25 @@ namespace ucpp::tags {
 
 struct no_tag;
 
-struct uart_tag{};
-struct gpio_tag{};
-struct sdmmc_tag{};
+template <typename T, typename tag>
+struct has_tag : std::is_same<typename T::tag, tag>
+{
+};
+
+template <typename T, typename tag>
+inline constexpr bool has_tag_v = has_tag<T, tag>::value;
+
+#define DECLARE_DEVICE(device) \
+struct device##_tag{}; \
+template <typename T> \
+using is_##device = has_tag<T,device##_tag>; \
+template <typename T> \
+inline constexpr bool is_##device##_v = is_##device<T>::value;
+
+
+DECLARE_DEVICE(uart)
+DECLARE_DEVICE(gpio)
+DECLARE_DEVICE(sdmmc)
 
 }
 
--- ucpp/stm32/rcc.hpp
+++ ucpp/stm32/rcc.hpp
@@ -1,41 +1,44 @@
 
 #pragma once
-#include <stdint.h>
+#include "../peripherals_tags.hpp"
 #include "../register.hpp"
 #include "rcc-regs.hpp"
-#include "../peripherals_tags.hpp"
+#include <stdint.h>
 
-namespace  {
-
-template<typename rcc_t, typename device_t>
-auto& clk_enable_bit(const rcc_t& rcc, const device_t&, typename std::enable_if_t<std::is_same_v<typename device_t::tag, ucpp::tags::gpio_tag>>* = 0)
+namespace
 {
-    if constexpr (device_t::index==0)
+using namespace ucpp::tags;
+template <typename rcc_t, typename device_t>
+auto& clk_enable_bit(const rcc_t& rcc, const device_t&,
+    typename std::enable_if_t<is_gpio_v<device_t>>* = 0)
+{
+    if constexpr (device_t::index == 0)
         return rcc.AHB1ENR.GPIOAEN;
-    if constexpr (device_t::index==1)
+    if constexpr (device_t::index == 1)
         return rcc.AHB1ENR.GPIOBEN;
-    if constexpr (device_t::index==2)
+    if constexpr (device_t::index == 2)
         return rcc.AHB1ENR.GPIOCEN;
-    if constexpr (device_t::index==3)
+    if constexpr (device_t::index == 3)
         return rcc.AHB1ENR.GPIODEN;
-    if constexpr (device_t::index==4)
+    if constexpr (device_t::index == 4)
         return rcc.AHB1ENR.GPIOEEN;
-    if constexpr (device_t::index==5)
+    if constexpr (device_t::index == 5)
         return rcc.AHB1ENR.GPIOFEN;
-    if constexpr (device_t::index==6)
+    if constexpr (device_t::index == 6)
         return rcc.AHB1ENR.GPIOGEN;
-    if constexpr (device_t::index==7)
+    if constexpr (device_t::index == 7)
         return rcc.AHB1ENR.GPIOHEN;
-    if constexpr (device_t::index==8)
+    if constexpr (device_t::index == 8)
         return rcc.AHB1ENR.GPIOIEN;
-    if constexpr (device_t::index==9)
+    if constexpr (device_t::index == 9)
         return rcc.AHB1ENR.GPIOJEN;
-    if constexpr (device_t::index==10)
+    if constexpr (device_t::index == 10)
         return rcc.AHB1ENR.GPIOKEN;
 }
 
-template<typename rcc_t, typename device_t>
-auto& clk_enable_bit(const rcc_t& rcc, const device_t&, typename std::enable_if_t<std::is_same_v<typename device_t::tag, ucpp::tags::sdmmc_tag>>* = 0)
+template <typename rcc_t, typename device_t>
+auto& clk_enable_bit(const rcc_t& rcc, const device_t&,
+    typename std::enable_if_t<is_sdmmc_v<device_t>>* = 0)
 {
     return rcc.APB2ENR.SDMMC1EN;
 }
@@ -45,10 +48,9 @@ auto& clk_enable_bit(const rcc_t& rcc, const device_t&, typename std::enable_if_
 using namespace ucpp::registers;
 namespace ucpp::stm32::rcc
 {
-template<typename rcc_type, typename device_t>
+template <typename rcc_type, typename device_t>
 void enable_clock(const rcc_type& rccdev, const device_t& device, bool state = true)
 {
-    clk_enable_bit(rccdev, device)=state;
+    clk_enable_bit(rccdev, device) = state;
 }
 }
-
